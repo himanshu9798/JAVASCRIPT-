69fb0503bdfa4f09c9d9bd246bcf3bfc29defbf9
himanshu kumar barnawal <129401939+himanshu9798@users.noreply.github.com>
2024-06-18 18:31:39 +0000

    container config done

---
+++ basic/datatypesummary.js
@@ -0,0 +1,16 @@
+// //primitive datatype
+// 6 types
+// a.string
+// b.number
+// c.boolean
+// d.null
+// e.undefined
+// f.symbol
+// g.bigint
+// //**  refernece type/non-primitive datatype*/
+// 1.array
+// 2.object
+// 3.functions
+
+
+// //JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them
