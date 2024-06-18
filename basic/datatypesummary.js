9c28e5e2aab7fba8a0a155ceabd20f7d7200d00b
himanshu kumar barnawal <129401939+himanshu9798@users.noreply.github.com>
2024-06-18 18:47:37 +0000

    container config done

--- basic/datatypesummary.js
+++ basic/datatypesummary.js
@@ -14,3 +14,20 @@
 
 
 // //JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them
+
+/*/********************
+types of memory
+1.stack memory(primitive)
+
+2.heap memory(non-primitive)
+
+
+
+*/
+let myname="himanshu";
+let newnm = myname;
+newnm ="barnawal";
+console.log(myname);
+console.log(newnm);
+
+ 
