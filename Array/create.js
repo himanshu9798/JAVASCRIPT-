90f1cfbc7bf39df87fffcd6c395374bbf2733083
himanshu kumar barnawal <129401939+himanshu9798@users.noreply.github.com>
2024-06-21 11:03:05 +0000

    container config done

--- Array/create.js
+++ Array/create.js
@@ -1,4 +1,21 @@
 let narr= [1,2,34,5,6];
 let name=["himanshu","vikash","laddu"];
 let arr=new Array(1,2,3,4);
-console.log(narr);
+// console.log(narr);
+// console.log(name)
+//array method
+// name.push("best freind")
+// console.log(name);
+// name.pop("laddu")
+// console.log(name);
+// name.pop("laddu");
+// console.log(name);
+// arr.unshift(9)
+// arr.shift();
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(3));
+const arra=arr.join();
+console.log(arra);
+console.log(arr);
+console.log( typeof arra);
+
